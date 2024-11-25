755075eed6993dda7c33eba6557c3d1878831e2f
パンダ <119229372+PxndxDev@users.noreply.github.com>
2024-11-25 23:57:13 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollContainer = document.getElementById("scroll-content");
+
+    // Aumenta la velocidad o ajustes adicionales si es necesario.
+    scrollContainer.style.animationDuration = "10s";
+});
